4ebb68a7237f27f95efead9a7ed022849ba7fe18
Karen <galstyankaren94@gmail.com>
2018-08-01 20:24:41 +0300

    animation changed

--- src/assets/js/app/app.js
+++ src/assets/js/app/app.js
@@ -1,5 +1,12 @@
+//custom
+$(document).ready(function(){
+
 // anime.js
-var basicTimeline = anime.timeline();
+var basicTimeline = anime.timeline({
+  complete: function(anim) {
+    // typed();
+  },
+});
 
 basicTimeline
   .add({
@@ -17,56 +24,19 @@ basicTimeline
   .add({
     targets: '.mac_animation .progress-bar',
     opacity: 1,
-    delay: 500,
+    complete: function(anim) {
+      loadProgressBar()
+      // $('.progress-bar').hide();
+    }
   })
 
   .add({
-    targets: '.ml5 .line',
-    opacity: [0.5,1],
-    scaleX: [0, 1],
-    easing: "easeInOutExpo",
-    duration: 700
-  }).add({
-    targets: '.ml5 .line',
-    duration: 600,
-    easing: "easeOutExpo",
-    translateY: function(e, i, l) {
-      var offset = -0.625 + 0.625*2*i;
-      return offset + "em";
-    }
-  }).add({
-    targets: '.ml5 .ampersand',
-    opacity: [0,1],
-    scaleY: [0.5, 1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=600'
-  }).add({
-    targets: '.ml5 .letters-left',
-    opacity: [0,1],
-    translateX: ["0.5em", 0],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=300'
-  }).add({
-    targets: '.ml5 .letters-right',
-    opacity: [0,1],
-    translateX: ["-0.5em", 0],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=600'
-  }).add({
-    targets: '.ml5',
+    targets: '.mac_animation .progress-bar',
     opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+    delay: 1500,
+  })
 
 
-
-//custom
-$(document).ready(function(){
   function move() {
     var elem = document.getElementById("bar");
     var width = 1;
@@ -79,18 +49,17 @@ $(document).ready(function(){
         elem.style.width = width + '%';
       }
     }
-    typed()
   };
 
   function loadProgressBar() {
-    $('.progress-bar span').slideDown("slow", move)
+    $('.progress-bar span').slideDown("slow", move);
   };
 
-  setTimeout(loadProgressBar, 1900);
+  // setTimeout(loadProgressBar, 1900);
 
 
   //typed.js
-  $(function typed() {
+  function typed() {
 
     var options = {
       stringsElement: '#typed-strings',
@@ -98,11 +67,9 @@ $(document).ready(function(){
       loop: false,
     }
 
-    $("#button").click(function () {
-      var typed = new Typed("#typed", options);
-    });
+    var typed = new Typed("#typed", options);
 
-  });
+  };
 
-
+  // setTimeout(typed, 2900);
 });
